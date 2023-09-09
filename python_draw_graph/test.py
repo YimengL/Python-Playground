2c3ef088d2bb476a0dd802a544eb4a0999ee8b69
yimengli <yimengcsu@gmail.com>
2023-09-10 00:44:24 +0200

    Update Draw Function

--- python_draw_graph/test.py
+++ python_draw_graph/test.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import csv
 
-# Start - Configuration
 
 file_name = 'data.csv'  # 文件位置
 
@@ -16,13 +15,9 @@ hyf_apikal = {'label': 'HyF-Apikal', 'data': []}
 hyf_mittel = {'label': 'HyF-Mittel', 'data': []}
 hyf_zervikal = {'label': 'HyF-Zertikal', 'data': []}
 
-# End Configuration
-
 full_data = [kf_apikal, kf_mittel, kf_zervikal,
              fm_apikal, fm_mittel, fm_zervikal,
              hyf_apikal, hyf_mittel, hyf_zervikal]
-
-color1 = "#EFE4E8"
 
 with open(file_name) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=',')
@@ -34,28 +29,15 @@ print("Please verify below data: ")
 for r in full_data:
     print("{}: {}".format(r['label'], r['data']))
 
-assert (len(full_data) == 9), "full data"
-
 data_to_plot = list(map(lambda d: d['data'], full_data))
 
-fs = 10
-pos = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# Create a figure instance
-# fig = plt.figure();
-# ax = fig.add_axes([0, 0, 1, 1])
+sns.stripplot(data=data_to_plot, palette='dark:black', edgecolor="gray", size=3)
 
-# ax.violinplot(data_to_plot, pos, points=20, widths=0.3,
-#                      showmeans=True, showextrema=True, showmedians=True)
-# ax.set_title('Custom violinplot 1', fontsize=fs)
-# # plt.show()
-
-
-plt.figure(dpi=120)
-plt.xlabel("Feilen-System", {"size": 10})
-plt.ylabel("Kanalstranport", {"size": 10})
-plt.yticks(fontsize=12)
-
-g = sns.violinplot(data_to_plot, palette=["#EFE4E8", 'r', 'r', 'g', 'g', 'g', 'b', 'b', 'b'])
-g.set_xticklabels(labels=list(map(lambda d: d['label'], full_data)), rotation=45)
+ax = sns.violinplot(data=data_to_plot)
+ax.set_title('Custom violinplot 1')
+ax.set_xlabel("Feilen-System")
+ax.set_ylabel("Kanalstranport")
+ax.set_xticklabels(labels=list(map(lambda d: d['label'], full_data)), rotation=45, fontsize=6)
+# palette=["#EFE4E8", 'r', 'r', 'g', 'g', 'g', 'b', 'b', 'b']
 
 plt.show()
